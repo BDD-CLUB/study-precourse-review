fda7bcdc4269fce565c82c6ace35e65e5729226c
이수빈 <02ggang9@gmail.com>
2023-11-05 16:07:41 +0900

    Delete src directory

--- src/main/java/lotto/Application.java
+++
@@ -1,9 +0,0 @@
-package lotto;
-
-import lotto.global.config.AppConfig;
-
-public class Application {
-    public static void main(String[] args) {
-        AppConfig.getLottoController().run();
-    }
-}
